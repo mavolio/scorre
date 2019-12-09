bfe4a46eef68c433304f266b6697564c7fe7653b
Meghan Avolio <mavolio2@jh.edu>
2019-12-09 11:30:35 +0100

    code to clean original try data - focused on a subset of possible traits

---
+++ merge_corre_try.R
@@ -0,0 +1,72 @@
+library(tidyverse)
+library(data.table)
+library(Hmisc)
+library(utf8)
+
+#from Habacuc
+
+setwd("C:/Users/mavolio2/Dropbox/converge_diverge/datasets/LongForm/fixing species names")
+setwd("C:/Users/megha/Dropbox/converge_diverge/datasets/LongForm/fixing species names")
+
+checkcorre<-read.csv("C:/Users/megha/Dropbox/converge_diverge/datasets/LongForm/SpeciesRelativeAbundance_March2019.csv")
+
+#load clean taxonomy for try
+taxdat <- read_csv("taxon_updates.csv")
+
+#get rid of species matched by taxize
+ii <- is.na(taxdat$species_Taxonstand) & !is.na(taxdat$species_taxize)
+
+taxdat$species_matched[ii] <- taxdat$species[ii]
+
+#select species and matched species columns
+taxdat %>%
+  select(species, species_matched) -> taxdat
+
+#load corre - not sure we have to do this, because the orgi
+corre <- read_csv("CoRRE_SpList_Sept2019.csv")
+#preprocessing to utf8
+corre %>%
+  mutate(species= as_utf8(genus_species))->corre
+#capitalize all records
+corre$species <- Hmisc::capitalize(tolower(corre$genus_species))
+
+#load taxonomy for corre
+taxcorre <- read_csv("Species_to_check_cleaned_2.csv")%>%
+  filter(remove==0)#this filters out only to sp., unknowns, and non-vasular plants except ferns.
+#get rid of taxize matches - we manually did all this
+# ii <- is.na(taxcorre$species_Taxonstand) & !is.na(taxcorre$species_taxize)
+# 
+# taxcorre$species_matched[ii] <- taxcorre$species[ii]
+#select submitted name and matched name
+taxcorre %>%
+  select(species, species_matched) -> taxcorre
+#join corre to updated taxonomy
+corre2 <- left_join(corre, taxcorre)%>%
+  select(-species)%>%
+  na.omit()
+
+#read try 
+try <- fread("TryAccSpecies.txt",sep = "\t",data.table = FALSE,stringsAsFactors = FALSE,strip.white = TRUE)
+#preprocess try
+try %>%
+  mutate(species= as_utf8(AccSpeciesName))->try
+#capitalize records
+try$species <- Hmisc::capitalize(tolower(try$AccSpeciesName))#why are we doing this?
+try$match<-ifelse(try$species==try$AccSpeciesName,1,0)
+
+#join try to updated taxonomu
+try <- left_join(try,taxdat, by = c("AccSpeciesName"="species"))%>%
+  select(-species)
+
+#join corre to try
+corre2try <- left_join(corre2,try, by="species_matched")
+
+write_csv(corre2try, path = "corre2trykey.csv")
+
+#make comma separted row to submit to try 
+
+try_list <- corre2try[["AccSpeciesID"]][!is.na(corre2try$AccSpeciesID)]
+
+write_delim(x = as.data.frame(t(try_list)), "list_for_try.csv",delim = ",",col_names = FALSE)
+
+
