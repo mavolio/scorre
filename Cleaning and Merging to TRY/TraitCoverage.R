3685a7fc98598b6804616be2e7181e1330c5dce2
Meghan Avolio <mavolio2@jh.edu>
2019-12-10 15:45:12 +0100

    Making Kaitlin's code compatible with Meghan's computer and dropping categorical traits

--- Cleaning and Merging to TRY/TraitCoverage.R
+++ Cleaning and Merging to TRY/TraitCoverage.R
@@ -12,10 +12,17 @@ library(tidyr)
 library(ggplot2)
 
 ## Load Data
-TRY_cat <- read.csv(here::here("Data", "TRY_trait_data_categorical.csv")) # categorical data
+TRY_cat <- read.csv(here::here("CoRRE Data", "TRY_trait_data_categorical.csv")) # categorical data
 TRY_con <- read.csv(here::here("Data", "TRY_trait_data_continuous.csv")) # continuous data
 Sp_lst <- read.csv(here::here("Data", "CoRRE_TRY_species_list.csv")) # need to use to combine trait data to CoRRE data
 Rel_abun <- read.csv(here::here("Data", "CoRRE_relative_abundance_Nov2019.csv"), row.names = 1) # Relative abundance data for coverage
+
+#meghan does not understand here
+
+#TRY_cat <- read.csv("C:/Users/mavolio2/Dropbox/Sdiv_Scorre_shared/CoRRE Data/TRY_trait_data_categorical.csv") # categorical data
+TRY_con <- read.csv("C:/Users/mavolio2/Dropbox/Sdiv_Scorre_shared/CoRRE Data/TRY_trait_data_continuous.csv") # continuous data
+Sp_lst <- read.csv("C:/Users/mavolio2/Dropbox/Sdiv_Scorre_shared/CoRRE Data/CoRRE_TRY_species_list.csv") # need to use to combine trait data to CoRRE data
+Rel_abun <- read.csv("C:/Users/mavolio2/Dropbox/Sdiv_Scorre_shared/CoRRE Data/CoRRE_relative_abundance_Nov2019.csv") # Relative abundance data for coverage
 
 
 # Make sp names consistent between TRY and CoRRE
@@ -27,19 +34,21 @@ test <- aggregate(Rel_abun$relcov, by = list(site_code = Rel_abun$site_code, pro
                                              calendar_year = Rel_abun$calendar_year, treatment_year = Rel_abun$treatment_year, treatment = Rel_abun$treatment, 
                                              block = Rel_abun$block, plot_id = Rel_abun$plot_id, species_matched = Rel_abun$species_matched), 
                   FUN = sum)
+
+
 names(test)[10] <- "relcov"
 ## Make dataframe with species as columns filled with relative abundances
 abundcov <- spread(test, species_matched, relcov)
 abundcov[is.na(abundcov)] <- 0
 
 ## Make dataframe with species as rows and traits as columns
-TRY_cat$present <- 1
-TRY_cat <- TRY_cat[,c(1,3,7)]
+#TRY_cat$present <- 1
+#TRY_cat <- TRY_cat[,c(1,3,7)]
 TRY_con$present <- 1
 TRY_con <- TRY_con[,c(1,3,7)]
-traits <- rbind(unique(TRY_cat), TRY_con) # dispersal mode is in TRY_cat x2
+traits <- TRY_con # dispersal mode is in TRY_cat x2
 traits <- spread(traits, CleanTraitName, present)
-traits <- traits[-which(is.na(traits$species_matched)),]
+#traits <- traits[-which(is.na(traits$species_matched)),] #not sure what this is doing and it is breakign the code
 traits <- traits[order(traits$species_matched),]
 rownames(traits) <- traits[,1]
 traits <- traits[,-1]
@@ -56,12 +65,31 @@ mult <- abund_mat %*% trait_mat
 # Add in site, project, ect
 mult <- cbind(abundcov[,c(1:8)], mult)
 
-long.mult <- gather(mult, trait, coverage, allelopathic:stomata_conductance)
-<<<<<<< HEAD
+#coverage by plot through time
+long.mult <- gather(mult, trait, coverage, LDMC:stomata_conductance)
+  
+
+write.csv(long.mult, "C:/Users/mavolio2/Dropbox/sDiv_sCoRRE_shared/CoRRE data/trait_coverage_byplot.csv", row.names=F)
+
 ## USE this df to look at subsets of interest. 
-=======
 
 ggplot(aes(x = site_code, y = coverage), data = long.mult) + 
   geom_boxplot() + 
   facet_grid(~trait)
->>>>>>> dd9161a5b754edbf6fa639c1d741e636c4742f43
+
+library(tidyverse)
+#site
+ave_site<-long.mult%>%
+  group_by(site_code, project_name, community_type, trait)%>%
+  summarize(mmcov=mean(coverage))%>%
+  ungroup%>%
+  group_by(site_code, project_name, trait)%>%
+  summarize(mmcov=mean(mcov))%>%
+  ungroup%>%
+  group_by(site_code, trait)%>%
+  summarize(mmcov=mean(mcov))
+  
+
+#geographic range
+
+
