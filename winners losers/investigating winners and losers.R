cb574e5d6cab768557825c30ad72b7c47eec6ebd
Meghan Avolio <mavolio2@jh.edu>
2020-09-17 13:50:01 -0400

    adding analyses to compare treatments and species. And doing this on a yearly basis instead of averaging over all years.

--- winners losers/investigating winners and losers.R
+++ winners losers/investigating winners and losers.R
@@ -129,7 +129,8 @@ grid.arrange(abundplot, log, ncol=2)
 
 
 ###for all the data
-trts<-read.csv("C:/Users/mavolio2/Dropbox/sDiv_sCoRRE_shared/CoRRE data/CoRRE_treatment_summary.csv")
+trts<-read.csv("C:/Users/mavolio2/Dropbox/sDiv_sCoRRE_shared/CoRRE data/CoRRE_treatment_summary.csv")%>%
+  select(-X)
 sp<-read.csv("C:/Users/mavolio2/Dropbox/sDiv_sCoRRE_shared/CoRRE data/CoRRE_TRY_species_list.csv")%>%
   select(genus_species, species_matched)%>%
   unique
@@ -193,7 +194,7 @@ grid.arrange(abundplot, log, ncol=2)
 allreldat<-reldat%>%
   select(-X)%>%
   left_join(trts)%>%
-  left_join(sp)%>%
+  left_join(sp)%>%# this drops the unknowns
   na.omit()
 
 relave<-allreldat%>%
@@ -281,13 +282,13 @@ treat_dom<-treat_freq%>%
 # grid.arrange(abundplot_dom, log_dom, ncol=2)
 
 CT<-treat_dom%>%
-  full_join(control_dom)
+  full_join(control_dom)#this introduces NA into the treatment column when it is present in the controls but not the treated plots.
 
 CT$DCi[is.na(CT$DCi)] <- 0
 CT$treatDCi[is.na(CT$treatDCi)] <- 0
 
 CT_plot<-CT%>%
-  mutate(spcolor=ifelse(species_matched=="Andropogon gerardii", "ag", ifelse(species_matched=="Ambrosia psilostachya", "ap", ifelse(species_matched=="Triodanis perfoliata", "tp", "a"))))
+  mutate(spcolor=ifelse(species_matched=="Andropogon gerardii", "ag", ifelse(species_matched=="Ambrosia psilostachya", "ap", ifelse(species_matched=="Linum sulcatum", "ls", "a"))))
 
 
 abundplot_dom2<-
@@ -309,32 +310,227 @@ residuals<-as.data.frame(with(toplot2_test2,(resid(lm(treatDCi-DCi~0)))))
 
 CT_diff<-CT%>%
   mutate(diff=treatDCi-DCi,
-         absdiff=abs(diff))%>%
-  select(-nplots, -ncplots, -freq, -cmean)
+         absdiff=abs(diff))
 
 categories<-CT_diff%>%
+  na.omit%>%
   mutate(cat=ifelse(DCi!=0&DCi<0.08&treatDCi>0.6, "Sup. Win.", 
              ifelse(DCi==0&treatDCi>0.6, "Sup. Dup. Win", 
-             ifelse(DCi>0.6&treatDCi==0, "Sup. Dup. Lose",
              ifelse(DCi>0.6&treatDCi!=0&treatDCi<0.1, "Sup. Lose",
              ifelse(DCi!=0&DCi<0.08&treatDCi!=0&treatDCi<0.08, "Rare Tol.", 
              ifelse(DCi>0.6&treatDCi>0.6, "Dom. Tol.", 
-             ifelse(DCi==0&treatDCi<0.8, "Appear Rare",
-             ifelse(DCi==0&treatDCi>0.8, "Appear", 
-             ifelse(DCi!=0&DCi<0.08&treatDCi==0,"Disapp. Rare",
-             ifelse(DCi>0.08&treatDCi==0, "Disapp.", 
+             ifelse(DCi==0&treatDCi<0.08, "Appear Rare",
+             ifelse(DCi==0&treatDCi>0.08, "Appear", 
+             ifelse(DCi>0.6&treatDCi<0.6, "Loose Dom.",
+             ifelse(DCi<0.08&treatDCi>0.08, "No Long. Rare",
              ifelse(diff>0.1, "Winner", 
              ifelse(absdiff>0.1&diff<0, "Loser", 
-             ifelse(absdiff<0.1, "Tol.", "none"))))))))))))),
+             ifelse(absdiff<0.1, "Tol.", "none")))))))))))),
          DR=ifelse(DCi>0.6, "Dom", 
-            ifelse(DCi!=0&DCi<0.08, "Rare", "none")))
+            ifelse(DCi!=0&DCi<0.08, "Rare", "none")),
+         AppDis=ifelse(DCi==0&treatDCi>0, "Appear",
+                ifelse(DCi>0&treatDCi==0, "Disapp.", "none")),
+         Pres=ifelse(DCi!=0&DCi>0.08&DCi<0.6, 1, 0))
+
+cat_sum<-categories%>%
+  group_by(cat)%>%
+  summarize(n=length(cat))%>%
+  mutate(prop=n/17670)
 
 dr_sum<-categories%>%
   group_by(DR)%>%
   summarize(n=length(DR))
 
-cat_sum<-categories%>%
+ad_sum<-categories%>%
+  group_by(AppDis)%>%
+  summarize(n=length(AppDis))
+
+sum(categories$Pres)
+
+###example with A. gerardii
+ange<-categories%>%
+  filter(species_matched=="Andropogon gerardii")
+
+angecat_sum<-ange%>%
   group_by(cat)%>%
   summarize(n=length(cat))%>%
-  mutate(prop=n/17984)
-         
+  mutate(prop=n/103)
+
+#common species
+common<-categories%>%
+  group_by(species_matched)%>%
+  summarize(n=length(species_matched))
+
+
+erca<-categories%>%
+  filter(species_matched=="Erigeron canadensis")
+
+ercacat_sum<-erca%>%
+  group_by(cat)%>%
+  summarize(n=length(cat))%>%
+  mutate(prop=n/152)
+
+popr<-categories%>%
+  filter(species_matched=="Poa pratensis")
+poprcat_sum<-popr%>%
+  group_by(cat)%>%
+  summarize(n=length(cat))%>%
+  mutate(prop=n/157)
+
+###contrasting n-addition versus drought
+trts2<-trts%>%
+  select(site_code, project_name, community_type, treatment, trt_type)%>%
+  unique
+
+
+cat_trts<-categories%>% #this is a problem for when species are lost b/c control has no treatment.
+  left_join(trts2)%>%
+  mutate(cat2=ifelse(cat=="Appear"|cat=="Winner"|cat=="No Long. Rare"|cat=="Appear Rare"|cat=="Sup. Dup. Win", "Winner", ifelse(cat=="Loser"|cat=="Loose Dom.", "Loser", ifelse(cat=="Tol."|cat=="Dom. Tol."|cat=="Rare Tol.", "Tol.", "None"))))
+
+ntrt<-cat_trts%>%
+  filter(trt_type=="N")
+ntrt_sum<-ntrt%>%
+  group_by(trt_type, cat2)%>%
+  summarize(n=length(cat2))%>%
+  mutate(prop=n/2249)
+
+drttrt<-cat_trts%>%
+  filter(trt_type=="drought")
+drttrt_sum<-drttrt%>%
+  group_by(trt_type,cat2)%>%
+  summarize(n=length(cat2))%>%
+  mutate(prop=n/939)
+
+irrtrt<-cat_trts%>%
+  filter(trt_type=="irr")
+irrtrt_sum<-irrtrt%>%
+  group_by(trt_type,cat2)%>%
+  summarize(n=length(cat2))%>%
+  mutate(prop=n/798)
+
+multnuttrt<-cat_trts%>%
+  filter(trt_type=="mult_nutrient")
+multnuttrt_sum<-multnuttrt%>%
+  group_by(trt_type,cat2)%>%
+  summarize(n=length(cat2))%>%
+  mutate(prop=n/3548)
+
+trt_contrast<-ntrt_sum%>%
+  bind_rows(drttrt_sum, irrtrt_sum, multnuttrt_sum)
+
+theme_set(theme_bw(16))
+
+ggplot(data=trt_contrast, aes(x=cat2, y=prop, fill=trt_type))+
+  geom_bar(stat="identity", position = position_dodge())+
+  xlab("Species Response")+
+  ylab("Proportion of Sp. Responses")+
+  scale_fill_manual(name="Treatment", values=c("lightblue", "darkblue",'darkred','red'))
+
+####now doing this for every year of an experiment.
+
+relave_yr<-allreldat%>%
+  group_by(site_code, project_name, community_type, treatment, plot_mani, treatment_year, species_matched)%>%
+  summarize(mean=mean(relcov))
+
+Crelave_yr<-relave_yr%>%
+  filter(plot_mani==0)%>%
+  ungroup()%>%
+  select(-treatment, -plot_mani)%>%
+  rename(cmean=mean)
+
+Trelave_yr<-relave_yr%>%
+  filter(plot_mani!=0)%>%
+  ungroup()%>%
+  select(-plot_mani)
+
+controlplots_yr<-allreldat%>%
+  filter(plot_mani==0)%>%
+  select(site_code, project_name, community_type, plot_id, treatment_year)%>%
+  unique()%>%
+  group_by(site_code, project_name, community_type, treatment_year)%>%
+  summarize(ncplots=length(plot_id))
+
+control_freq_yr<-allreldat%>%
+  filter(plot_mani==0)%>%
+  select(site_code, project_name, community_type, species_matched, plot_id, treatment_year)%>%
+  unique()%>%
+  group_by(site_code, project_name, community_type, species_matched, treatment_year)%>%
+  summarize(nplots=length(plot_id))%>%
+  left_join(controlplots)%>%
+  mutate(freq=nplots/ncplots)
+
+control_dom_yr<-control_freq_yr%>%
+  left_join(Crelave_yr)%>%
+  mutate(DCi=(cmean+freq)/2)%>%
+  select(site_code, project_name, community_type, species_matched, treatment_year, DCi)
+
+treatplots_yr<-allreldat%>%
+  filter(plot_mani!=0)%>%
+  select(site_code, project_name, community_type, treatment, plot_id, treatment_year)%>%
+  unique()%>%
+  group_by(site_code, project_name, community_type, treatment, treatment_year)%>%
+  summarize(ntplots=length(plot_id))
+
+treat_freq_yr<-allreldat%>%
+  filter(plot_mani!=0)%>%
+  select(site_code, project_name, community_type, treatment, species_matched, plot_id, treatment_year)%>%
+  unique()%>%
+  group_by(site_code, project_name, community_type, treatment, species_matched, treatment_year)%>%
+  summarize(nplots=length(plot_id))%>%
+  left_join(treatplots)%>%
+  mutate(freq=nplots/ntplots)
+
+treat_dom_yr<-treat_freq_yr%>%
+  left_join(Trelave_yr)%>%
+  mutate(treatDCi=(mean+freq)/2)%>%
+  select(site_code, project_name, community_type, treatment, species_matched, treatment_year, treatDCi)
+
+CT_yr<-treat_dom_yr%>%
+  full_join(control_dom_yr)#this introduces NA into the treatment column when it is present in the controls but not the treated plots.
+
+CT_yr$DCi[is.na(CT_yr$DCi)] <- 0
+CT_yr$treatDCi[is.na(CT_yr$treatDCi)] <- 0
+
+
+CT_diff_yr<-CT_yr%>%
+  mutate(diff=treatDCi-DCi,
+         absdiff=abs(diff))
+
+categories_yr<-CT_diff_yr%>%
+  na.omit%>%
+  mutate(cat=ifelse(DCi!=0&DCi<0.08&treatDCi>0.6, "Sup. Win.", 
+             ifelse(DCi==0&treatDCi>0.6, "Sup. Dup. Win", 
+             ifelse(DCi>0.6&treatDCi!=0&treatDCi<0.1, "Sup. Lose",
+             ifelse(DCi!=0&DCi<0.08&treatDCi!=0&treatDCi<0.08, "Rare Tol.", 
+             ifelse(DCi>0.6&treatDCi>0.6, "Dom. Tol.", 
+             ifelse(DCi==0&treatDCi<0.08, "Appear Rare",
+             ifelse(DCi==0&treatDCi>0.08, "Appear", 
+             ifelse(DCi>0.6&treatDCi<0.6, "Loose Dom.",
+             ifelse(DCi<0.08&treatDCi>0.08, "No Long. Rare",
+             ifelse(diff>0.1, "Winner", 
+             ifelse(absdiff>0.1&diff<0, "Loser", 
+             ifelse(absdiff<0.1, "Tol.", "none")))))))))))),
+         DR=ifelse(DCi>0.6, "Dom", 
+                   ifelse(DCi!=0&DCi<0.08, "Rare", "none")),
+         AppDis=ifelse(DCi==0&treatDCi>0, "Appear",
+                       ifelse(DCi>0&treatDCi==0, "Disapp.", "none")),
+         Pres=ifelse(DCi!=0&DCi>0.08&DCi<0.6, 1, 0),
+         cat2=ifelse(cat=="Appear"|cat=="Winner"|cat=="No Long. Rare"|cat=="Appear Rare"|cat=="Sup. Dup. Win", "Winner", ifelse(cat=="Loser"|cat=="Loose Dom."|cat=="Sup. Lose", "Loser", ifelse(cat=="Tol."|cat=="Dom. Tol."|cat=="Rare Tol.", "Tol.", "None"))))
+
+cat_sum_yr<-categories_yr%>%
+  group_by(cat)%>%
+  summarize(n=length(cat))%>%
+  mutate(prop=n/72221)
+
+dr_sum_yr<-categories_yr%>%
+  group_by(DR)%>%
+  summarize(n=length(DR))
+
+ad_sum_yr<-categories_yr%>%
+  group_by(AppDis)%>%
+  summarize(n=length(AppDis))
+
+cat2_sum_yr<-categories_yr%>%
+  group_by(cat2)%>%
+  summarise(n=length(cat2))%>%
+  mutate(prop=n/72221)
