43718f7474616d60b1e0a233139c5e3c9e4c8d26
wilcoxkr <wilcoxkr@gmail.com>
2021-03-08 11:49:39 -0700

    starting new script

---
+++ winners losers/mathematical patterns of DCi.R
@@ -0,0 +1,6 @@
+### Looking at mathematical patterns among DCi, abundance, and frequency
+###
+### Author: Kevin Wilcox (kevin.wilcox@uwyo.edu)
+### Created March 8, 2021; last updated March 8, 2021
+
+###
