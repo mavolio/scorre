f56008753fb8219f29e701c2ca9d537b67dfad18
Timothy Ohlert <49884319+tim-ohlert@users.noreply.github.com>
2021-03-10 12:37:34 -0700

    Tim's work to prep for model loop

--- Phylogenetic Analyses/sCoRRE_mixedeffectmods.R
+++ Phylogenetic Analyses/sCoRRE_mixedeffectmods.R
@@ -3,10 +3,12 @@
 library(tidyverse)
 library(gridExtra)
 library(nlme)
+library(lmerTest)
 ###read in data
 
-my.wd <- "~/Dropbox/sDiv_sCoRRE_shared/"
+#my.wd <- "~/Dropbox/sDiv_sCoRRE_shared/"
 #my.wd <- "C:/Users/mavolio2/Dropbox/sDiv_sCoRRE_shared/"
+my.wd <- "C:/Users/ohler/Dropbox/sDiv_sCoRRE_shared/"
 
 #read in the data
 
@@ -15,10 +17,10 @@ my.wd <- "~/Dropbox/sDiv_sCoRRE_shared/"
 #relative abundance data
 #reldat<-read.csv(paste(my.wd, "CoRRE data/CoRRE data/community composition/CoRRE_RelativeAbundance_Feb2021.csv",sep=""))
 # PD data
-pd <- read.csv(paste(my.wd, "paper 2_PD and FD responses/data/CoRRE_pd_metrics_nonweighted_mg.csv", sep = ""))
+pd <- read.csv(paste(my.wd, "paper 2_PD and FD responses/data/CoRRE_pd_metrics_nonweighted_mg.csv", sep = ""), fileEncoding = "UTF-8-BOM")
 
 trt <- read.csv(paste(my.wd,'CoRRE data/CoRRE data/community composition/CoRRE_RawAbundanceMar2021.csv', sep = ""))%>%
-  select(site_code, project_name, community_type, treatment_year, calendar_year, treatment, plot_id)%>%
+  select(site_code, project_name, community_type, treatment_year, calendar_year, treatment, plot_id, block)%>%
   unique()
 
 trts<-read.csv(paste(my.wd, "CoRRE data/CoRRE data/community composition/CoRRE_ExperimentInfoMar2021.csv", sep=""))%>%
@@ -27,7 +29,8 @@ trts<-read.csv(paste(my.wd, "CoRRE data/CoRRE data/community composition/CoRRE_E
   unique()
 
 trt_analysis<-trts%>%
-  mutate(alltrts=ifelse(trt_type %in% c("CO2","CO2*temp", "mow_clip","burn","burn*graze","disturbance","burn*mow_clip","drought","drought*CO2*temp","drought*mow_clip","drought*temp*mow_clip","herb_removal","herb_removal*mow_clip","irr*CO2","irr*CO2*temp","irr*mow_clip","irr*herb_removal","irr*temp*mow_clip","N*CO2*temp","N*irr*CO2","N*irr*mow_clip","N*P*burn*graze", "mult_nutrient*irr","N*irr*CO2*temp", "N","mult_nutrient","N*P","P","N*CO2","N*mow_clip","N*burn","N*burn*graze","N*disturbance","P*burn*graze","P*burn*mow_clip","N*drought","N*herb_removal","P*herb_removal","N*irr","N*irr*temp","N*temp","mult_nutrient*temp","N*P*temp","mult_nutrient*mow_clip","N*burn*mow_clip","N*P*burn","N*P*mow_clip","P*burn","P*mow_clip","mult_nutrient*herb_removal","mult_nutrient*herb_removal*mow_clip","temp","temp*mow_clip","drought*temp","irr*temp","irr"),1,0))%>%
+  mutate(control = ifelse(trt_type == "control", 1,0))%>%
+  mutate(alltrts=ifelse(trt_type %in% c("CO2","CO2*temp", "mow_clip","burn","burn*graze","disturbance","burn*mow_clip","drought","drought*CO2*temp","drought*mow_clip","drought*temp*mow_clip","herb_removal","herb_removal*mow_clip","irr*CO2","irr*CO2*temp","irr*mow_clip","irr*herb_removal","irr*temp*mow_clip","N*CO2*temp","N*irr*CO2","N*irr*mow_clip","N*P*burn*graze", "mult_nutrient*irr","N*irr*CO2*temp", "N","mult_nutrient","N*P","P","N*CO2","N*mow_clip","N*burn","N*burn*graze","N*disturbance","P*burn*graze","P*burn*mow_clip","N*drought","N*herb_removal","P*herb_removal","N*irr","N*irr*temp","N*temp","mult_nutrient*temp","N*P*temp","mult_nutrient*mow_clip","N*burn*mow_clip","N*P*burn","N*P*mow_clip","P*burn","P*mow_clip","mult_nutrient*herb_removal","mult_nutrient*herb_removal*mow_clip","temp","temp*mow_clip","drought*temp","irr*temp","irr", "control"),1,0))%>%
   filter(alltrts==1)%>%
   mutate(dist=ifelse(trt_type %in% c("mow_clip","burn","burn*graze","disturbance","burn*mow_clip"), 1, 0), 
          dist_other=ifelse(trt_type %in% c("drought*mow_clip","drought*temp*mow_clip", "irr*mow_clip","irr*temp*mow_clip","N*irr*mow_clip","N*P*burn*graze","N*mow_clip","N*burn","N*burn*graze","N*disturbance","P*burn*graze","P*burn*mow_clip", "mult_nutrient*mow_clip","N*burn*mow_clip", "N*P*burn","N*P*mow_clip","P*burn","P*mow_clip","mult_nutrient*herb_removal","mult_nutrient*herb_removal*mow_clip","temp*mow_clip"), 1, 0),
@@ -45,7 +48,7 @@ trt_analysis<-trts%>%
          nuts_other=ifelse(trt_type %in% c("control","N*CO2*temp","N*irr*CO2","N*irr*mow_clip","N*P*burn*graze","mult_nutrient*irr","N*irr*CO2*tempN*CO2","N*mow_clip","N*burn","N*burn*graze","N*disturbance","P*burn*graze","P*burn*mow_clip","N*drought","N*herb_removal","P*herb_removal","N*irr","N*irr*temp","N*temp","mult_nutrient*temp","N*P*temp","mult_nutrient*mow_clip","N*burn*mow_clip","N*P*burn","N*P*mow_clip","P*burn","P*mow_clip","mult_nutrient*herb_removal","mult_nutrient*herb_removal*mow_clip"), 1, 0),
          multtrts=ifelse(trt_type %in% c("CO2*temp", "burn*graze","burn*mow_clip","drought*CO2*temp","drought*mow_clip","drought*temp*mow_clip","herb_removal*mow_clip","irr*CO2","irr*CO2*temp","irr*mow_clip","irr*herb_removal","irr*temp*mow_clip","N*CO2*temp","N*irr*CO2","N*irr*mow_clip","N*P*burn*graze", "mult_nutrient*irr","N*irr*CO2*temp", "N*CO2","N*mow_clip","N*burn","N*burn*graze","N*disturbance","P*burn*graze","P*burn*mow_clip","N*drought","N*herb_removal","P*herb_removal","N*irr","N*irr*temp","N*temp","mult_nutrient*temp","N*P*temp","mult_nutrient*mow_clip","N*burn*mow_clip","N*P*burn","N*P*mow_clip","P*burn","P*mow_clip","mult_nutrient*herb_removal","mult_nutrient*herb_removal*mow_clip","temp*mow_clip","drought*temp","irr*temp"),1,0))
 
-dat2<-do.call(rbind, strsplit(pd$plot.id, "[.]"))
+dat2<-do.call(rbind, strsplit(pd$plot.id, "\\."))
 dat3 <- as.data.frame(dat2)
 dat3$plot.id <- pd$plot.id
 dat4 <- dat3[which(dat3$V1 != dat3$V6),]
@@ -63,9 +66,29 @@ sites <- rbind(dat3, dat4, nutnet)
 
 newdf <- merge(pd, sites)
 
+newdf1 <- merge(newdf, trt)
+newdf2 <- merge(newdf1,trt_analysis)
+newdf2$experimentid <- paste(newdf2$site_code, newdf2$project_name, newdf2$community_type, sep = ",")
+
+
+test <- newdf2%>%
+  subset(trt_type == "N")
+
+expvector <- unique(test$experimentid)
+test2 <- newdf2%>%
+  subset(trt_type == "control" & experimentid %in% expvector)
+
+test3 <- rbind(test, test2)
 
 
 
+
+
+
+mod <- lmer(pd.raw ~ treatment + (1|treatment_year) + (1|plot_id), test)
+  summary(mod)   
+     
+     
 mod <- lme(pd ~ treatment, data = df, random = 1/plot.id)
 
 
