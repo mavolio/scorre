11d4fc84413012288be0585a8a3fcdacc37f5595
Meghan <meghan.avolio@gmail.com>
2021-04-14 12:01:48 -0400

    adding in the mixed models.

--- winners losers/graph of traits by treatments.R
+++ winners losers/graph of traits by treatments.R
@@ -1,4 +1,5 @@
 library(tidyverse)
+library(lme4)
 
 ### Trait data
 
@@ -21,7 +22,7 @@ barGraphStats <- function(data, variable, byFactorNames) {
 }  
 
 #read in data
-contTraits <- read.csv('C:/Users/mavolio2/Dropbox/sDiv_sCoRRE_shared/Trait Data/TRY Data/Gap_Filled/TRY_new.csv')%>%
+contTraits <- read.csv('E:/Dropbox/sDiv_sCoRRE_shared/Trait Data/TRY Data/Gap_Filled/TRY_new.csv')%>%
   rename(species_matched=Species)%>%
   select(-X.1, -X, -Family, -Genus, -ObservationID)%>%
   group_by(species_matched)%>%
@@ -32,7 +33,7 @@ contTraitsSubset <- contTraits%>%
   rename(ssd=X4, rooting_depth=X6, SLA=X11, leaf_C_mass=X13, leaf_N_mass=X14, leaf_P_mass=X15, stem_diameter=X21, seed_mass=X26, seed_length=X27, leaf_thickness=X46, LDMC=X47, leaf_dry_mass=X55, germination_rate=X95, leaf_length=X144, leaf_width=X145, leaf_CN=X146, stem_conduit_density=X169, stem_conduit_diameter=X281, seed_number=X138, SRL=X1080)%>%
   select(-X18, -X50, -X78, -X163, -X223, -X224, -X237, -X282, -X289, -X3112, -X3113, -X3114, -X3120)
 
-traits <- read.csv('C:/Users/mavolio2/Dropbox/sDiv_sCoRRE_shared/CoRRE data\\CoRRE data\\trait data\\sCoRRE categorical trait data - traits_complete_pre spot check_03102021.csv')%>%
+traits <- read.csv('E:/Dropbox/sDiv_sCoRRE_shared/CoRRE data\\CoRRE data\\trait data\\sCoRRE categorical trait data - traits_complete_pre spot check_03102021.csv')%>%
   full_join(contTraitsSubset) %>%
   drop_na()%>%
   filter(leaf_P_mass<20, stem_diameter<0.5, seed_mass<50, seed_number<10000, leaf_width<40, stem_conduit_density<1000, stem_conduit_diameter<200)
@@ -47,28 +48,62 @@ traitsScaled <- traitsOutliersRemoved %>% ## only scales continuous traits
 
 # Read in dci diff
 
-dcidiff<-read.csv("C:/Users/mavolio2/Dropbox/sDiv_sCoRRE_shared/WinnersLosers paper/data/Species_DCiDiff_newtrts.csv")
+# dcidiff<-read.csv("C:/Users/megha/Dropbox/sDiv_sCoRRE_shared/WinnersLosers paper/data/Species_DCiDiff_newtrts.csv")
 
-alldat_cont<-dcidiff%>%
+dcidiff_models<-read.csv("E:/Dropbox/sDiv_sCoRRE_shared/WinnersLosers paper/data/Species_DCiDiff_formixedmodels.csv")
+
+alldat_cont<-dcidiff_models%>%
   right_join(traitsScaled)%>%
   select(-leaf_type, -leaf_compoundness, -growth_form, -photosynthetic_pathway, -lifespan, -stem_support, -clonal, -mycorrhizal, -mycorrhizal_type, -n_fixation, -rhizobial,-actinorhizal)%>%
   filter(seed_mass<4, seed_number<3) %>%
   gather(ssd:SRL, key="trait", value="value")%>%
   na.omit()%>%
-  filter(trt_type2!="co2_other"&trt_type2!="dist_other"&trt_type2!="drt_other"&trt_type2!="herb_rem_other"&trt_type2!="irg_other"&trt_type2!="nuts_other"&trt_type2!="temp_other")%>%
-  filter(trait %in% c("seed_mass", "seed_number", "rooting_depth", "SRL", "LDMC", "SLA"))
+  filter(trait %in% c("seed_mass", "seed_number", "rooting_depth", "SRL", "LDMC", "SLA"))%>%
+  filter(trait=="SRL", trt_type2=="n"|trt_type2=="all mult"|trt_type2=="p")
 
-ggplot(data=alldat_cont, aes(x=value, y=ave_diff))+
-  geom_point()+
-  geom_smooth(method="lm")+
-  facet_grid(trt_type2~trait, scales="free")
+# ggplot(data=alldat_cont, aes(x=value, y=ave_diff))+
+#   geom_point()+
+#   geom_smooth(method="lm")+
+#   facet_grid(trt_type2~trait, scales="free")
+####
+#making mixed models
 
-alldat_cat<-dcidiff%>%
+m1<-lmer(diff ~ -1 + trt_type2 + value:trt_type2 + (trt_type2|species_matched) + (trt_type2|site_code), data=alldat_cont)
+
+
+##-1 says don't give me an overall intercept. just changing the output. I want an intercept per level of each trt_type2
+##fit a seperate slope for the value term for each of trt_type levels
+## by using trt|species - come up with a value for each species, but do that for each trt type category
+names(summary(m1))
+
+plot<-as.data.frame(summary(m1)$coefficients)
+fixef(m1) #should give fixed effects of model
+
+
+alldat_cat<-dcidiff_mult_n%>%
   right_join(traitsScaled)%>%
-  select(species_matched, trt_type2, ave_diff, leaf_type, leaf_compoundness, growth_form, photosynthetic_pathway, lifespan, stem_support, clonal,mycorrhizal, mycorrhizal_type, n_fixation, rhizobial,actinorhizal)%>%
+  select(species_matched, trt_type2, site_code, diff, leaf_type, leaf_compoundness, growth_form, photosynthetic_pathway, lifespan, stem_support, clonal,mycorrhizal, mycorrhizal_type, n_fixation, rhizobial,actinorhizal)%>%
   gather(leaf_type:actinorhizal, key="trait", value="value")%>%
   na.omit()%>%
-  filter(trt_type2!="co2_other"&trt_type2!="dist_other"&trt_type2!="drt_other"&trt_type2!="herb_rem_other"&trt_type2!="irg_other"&trt_type2!="nuts_other"&trt_type2!="temp_other")%>%
+  filter(trait=="photosynthetic_pathway")
+
+
+m2<-lmer(diff ~ -1 + trt_type2 + value:trt_type2 + (trt_type2|species_matched) + (trt_type2|site_code), data=alldat_cat)
+
+m2_b = lmer(diff ~ -1 + trt_type2 +  (trt_type2|species_matched) + (trt_type2|site_code), data=alldat_cat)
+
+
+#ways to try to get the estimates easily from the categorical models.
+anova.lme()
+
+summary(m2_b)
+anova(m2)
+aov(m2)
+
+contrasts(as.factor(alldat_cat$value))
+emmeans(m2, ~ value*treat_type)
+
+#  filter(trt_type2!="co2_other"&trt_type2!="dist_other"&trt_type2!="drt_other"&trt_type2!="herb_rem_other"&trt_type2!="irg_other"&trt_type2!="nuts_other"&trt_type2!="temp_other")%>%
   filter(trait %in% c("lifespan", "clonal", "photosynthetic_pathway", "mycorrhizal", "n_fixation", "growth_form"))
 
 ggplot(data=barGraphStats(data=subset(alldat_cat, ave_diff>0), variable="ave_diff", byFactorNames=c("trait", "trt_type2", "value")), aes(x=value, y=mean))+
