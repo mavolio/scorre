da12a3a7179ebc212410c7c46ce7345c42b27883
Meghan <meghan.avolio@gmail.com>
2021-04-16 09:43:40 -0400

    adding the n+other treatment

--- winners losers/DCi responses.R
+++ winners losers/DCi responses.R
@@ -41,6 +41,7 @@ trt_analysis<-trts%>%
          temp_other=ifelse(trt_type %in% c("control","CO2*temp","drought*CO2*temp","drought*temp*mow_clip","irr*CO2*temp","irr*temp*mow_clip","N*CO2*temp","N*irr*CO2*temp","N*irr*temp","N*temp","mult_nutrient*temp","N*P*temp","temp*mow_clip","drought*temp","irr*temp"), 1, 0),
          nuts_other=ifelse(trt_type %in% c("control","N*CO2*temp","N*irr*CO2","N*irr*mow_clip","N*P*burn*graze","mult_nutrient*irr","N*irr*CO2*tempN*CO2","N*mow_clip","N*burn","N*burn*graze","N*disturbance","P*burn*graze","P*burn*mow_clip","N*drought","N*herb_removal","P*herb_removal","N*irr","N*irr*temp","N*temp","mult_nutrient*temp","N*P*temp","mult_nutrient*mow_clip","N*burn*mow_clip","N*P*burn","N*P*mow_clip","P*burn","P*mow_clip","mult_nutrient*herb_removal","mult_nutrient*herb_removal*mow_clip"), 1, 0),
          n=ifelse(trt_type=="N", 1, 0),
+         n_other=ifelse(trt_type %in% c("N*CO2*temp","N*irr*CO2","N*irr*mow_clip","N*P*burn*graze","N*irr*CO2*tempN*CO2","N*mow_clip","N*burn","N*burn*graze","N*disturbance","N*drought","N*herb_removal","N*irr","N*irr*temp","N*temp","N*P*temp","N*burn*mow_clip","N*P*burn","N*P*mow_clip"), 1, 0),
          p=ifelse(trt_type=="P", 1, 0),
          multtrts=ifelse(trt_type %in% c("mult_nutrient","N*P","CO2*temp", "burn*graze","burn*mow_clip","drought*CO2*temp","drought*mow_clip","drought*temp*mow_clip","herb_removal*mow_clip","irr*CO2","irr*CO2*temp","irr*mow_clip","irr*herb_removal","irr*temp*mow_clip","N*CO2*temp","N*irr*CO2","N*irr*mow_clip","N*P*burn*graze", "mult_nutrient*irr","N*irr*CO2*temp", "N*CO2","N*mow_clip","N*burn","N*burn*graze","N*disturbance","P*burn*graze","P*burn*mow_clip","N*drought","N*herb_removal","P*herb_removal","N*irr","N*irr*temp","N*temp","mult_nutrient*temp","N*P*temp","mult_nutrient*mow_clip","N*burn*mow_clip","N*P*burn","N*P*mow_clip","P*burn","P*mow_clip","mult_nutrient*herb_removal","mult_nutrient*herb_removal*mow_clip","temp*mow_clip","drought*temp","irr*temp"),1,0))
 
@@ -376,6 +377,17 @@ CT_Sp_nuts_other<-CT_diff%>%
   mutate(se=sd/sqrt(nobs))%>%
   mutate(trt_type2="nuts_other")
 
+CT_Sp_n_other<-CT_diff%>%
+  filter(n_other==1)%>%
+  group_by(species_matched)%>%
+  summarize(ave_diff=mean(diff),
+            nobs=length(diff),
+            sd=sd(diff),
+            min=min(diff),
+            max=max(diff))%>%
+  mutate(se=sd/sqrt(nobs))%>%
+  mutate(trt_type2="n_other")
+
 CT_Sp_allint<-CT_diff%>%
   filter(multtrts==1)%>%
   group_by(species_matched)%>%
@@ -388,7 +400,7 @@ CT_Sp_allint<-CT_diff%>%
   mutate(trt_type2="all mult")
 
 Fulldataset<-CT_Sp_allint%>%
-  bind_rows(CT_Sp_co2, CT_Sp_dist, CT_Sp_drt, CT_Sp_herb, CT_Sp_irg, CT_Sp_N, CT_Sp_P, CT_Sp_temp)%>%
+  bind_rows(CT_Sp_co2, CT_Sp_dist, CT_Sp_drt, CT_Sp_herb, CT_Sp_irg, CT_Sp_N,CT_Sp_n_other, CT_Sp_P, CT_Sp_temp)%>%
   select(species_matched, trt_type2, nobs, ave_diff, min, max, se)
 
 write.csv(Fulldataset, paste(my.wd, "WinnersLosers paper/data/Species_DCiDiff_newtrts.csv", sep=""), row.names=F)
