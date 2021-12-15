fe89b81c24317432bff62079461dcb6c7b2cd198
Timothy Ohlert <49884319+tim-ohlert@users.noreply.github.com>
2021-12-15 08:56:18 -0700

    getting the distances working

--- ConvergenceDivergence/tim_convergencedivergence.R
+++ ConvergenceDivergence/tim_convergencedivergence.R
@@ -85,8 +85,9 @@ nyear <- experimentinfo[c("site_code", "project_name", "community_type", "treatm
         subset(treatment_year != 0)%>%
         ddply(.(site_code, project_name, community_type),
               function(x)data.frame(
-                n.trt.yrs = length(x$treatment_year)
+                n.trt.yrs = max(x$treatment_year)
               ))
+
 
 
 
@@ -116,31 +117,36 @@ test <- cover %>%
 test <- test %>%
         subset( n.trt.yrs >=10)%>%
         subset(last_trt_yr == calendar_year)%>%
-  subset( trt_type == "control" | trt_type == "N" | trt_type == "P" | trt_type == "irr" | trt_type == "drought")
+  subset( trt_type == "control" | trt_type == "N" | trt_type == "P" | trt_type == "irr" | 
+            trt_type == "drought")%>%
+  subset(site_code == "KNZ" | 
+    #site_code == "NWT" | 
+      site_code == "SEV"
+    )##THIS IS ONLY TO GET THE CODE TO RUN FASTER WHILE MAKING THE WORKFLOW
 
 test <- test[c("site_code", "project_name", "community_type", "treatment_year", "plot_id", "genus_species", "relcov", "trt_type")]%>%
         unique()
 
 
-
-
+plot.treatment <- test[c("site_code", "project_name", "community_type", "plot_id", "trt_type")]%>%
+                  unique()
+plot.treatment <- tidyr::unite(plot.treatment, "rep", c("site_code", "project_name", "community_type", "plot_id"), sep = "::", remove = FALSE)
 
 df <- merge(test, traits, by.x = "genus_species", by.y = "species_matched", all.x = TRUE)
 
-df <- unite(df, rep, c("site_code", "project_name", "community_type", "plot_id"), sep = "-")
+df <- unite(df, rep, c("site_code", "project_name", "community_type", "plot_id"), sep = "::")
 
 df$ok <- complete.cases(df[,c("seed_dry_mass", 
-                             "stem_spec_density",
-                             "leaf_N",
-                             "leaf_P",
-                             "LDMC")])
+                              "LDMC",
+                              "plant_height_vegetative",
+                              "rooting_depth"
+                              )])
 df <- subset(df, ok == TRUE)
 
 hv_split <- base::split(df[,c("seed_dry_mass", 
-                              "stem_spec_density",
-                              "leaf_N",
-                              "leaf_P",
                               "LDMC",
+                              "plant_height_vegetative",
+                              "rooting_depth",
                               "relcov",
                               "rep")], df$rep)
 hv_split <- subset(hv_split, lapply(hv_split, nrow) >1)
@@ -150,7 +156,7 @@ hv_split <- subset(hv_split, lapply(hv_split, nrow) >1)
 
 
 hv_func <- function(x) {
-  hypervolume_gaussian(data = x[1:3], name = unique(x$rep), weight = x$relcov, 
+  hypervolume_gaussian(data = x[1:4], name = unique(x$rep), weight = x$relcov, 
                        verbose = FALSE) 
   }
 
@@ -190,15 +196,79 @@ hvs_joined = hypervolume_join(hv_volumes)
 
 
 
-alpha <- kernel.alpha(hvs_joined)
-evenness <- kernel.evenness(hvs_joined)
-dispersion <- kernel.dispersion(hvs_joined)
+#alpha <- kernel.alpha(hvs_joined)
+#evenness <- kernel.evenness(hvs_joined)
+#dispersion <- kernel.dispersion(hvs_joined)
 
 
-rep <- names(hv_volumes)
-hv_div <- data.frame(rep, alpha)
+#rep <- names(hv_volumes)
+#hv_div <- data.frame(rep, alpha, evenness, dispersion)
 
-hv_div <- separate(hv_div, rep, c("site_code", "project_name", "community_type", "plot_id"), sep = "-")
+#hv_div <- separate(hv_div, rep, c("site_code", "project_name", "community_type", "plot_id"), sep = "-")
+
+#hv_div <- unite(hv_div, expgroup, c("site_code", "project_name", "community_type"), sep = "-", remove = FALSE)
+
+#hv_div <- merge(hv_div, plot.treatment, by = c("site_code", "project_name", "community_type", "plot_id"))
+
+
+#ggplot(hv_div, aes(x=trt_type, y = alpha))+
+#  facet_wrap(~expgroup)+
+#  geom_boxplot()+
+#  ylim(0,3)+
+#  theme_bw()
+
+
+#dist_func <- function(x) {
+#  hypervolume_distance(hv1 = x, hv2 = (x+1)) 
+#}
+
+#lapply(hv_volumes, dist_func)
+
+
+distance.measures <- kernel.similarity(hvs_joined) #this takes a long time, but once it's done I hope to be able to subset dataframe by sites and treatments which is the last step for comparison
+
+#distance.centroids <- distance$Distance_centroids
+
+library(reshape2)
+
+distance.centroids.dat <- melt(as.matrix(distance.measures$Distance_centroids), varnames = c("rep1", "rep2"))
+
+
+
+scooby <- merge(distance.centroids.dat, plot.treatment, by.x = "rep1", by.y = "rep")
+dooby <- scooby[c("rep1", "rep2", "value", "trt_type", "site_code", "project_name", "community_type")]
+dooby <- unite(dooby, expgroup1, c("site_code", "project_name", "community_type"))
+dooby$trt_rep1 <- dooby$trt_type
+
+doo <- merge(dooby, plot.treatment, by.x = "rep2", by.y = "rep")
+doo <- unite(doo, expgroup2, c("site_code", "project_name", "community_type"))
+doo$trt_rep2 <- doo$trt_type.y
+
+doo <- doo[c("rep1", "rep2", "value", "trt_rep1", "trt_rep2", "expgroup1", "expgroup2")]
+
+
+credits <- unite(doo, trt_comp, c("trt_rep1", "trt_rep2"), sep = ".", remove = FALSE)
+
+goingon <- subset(credits, value != 0)
+
+last <- subset(goingon, trt_comp == "control.control" | trt_comp == "drought.drought")
+
+
+past <- last%>%
+  mutate( match = ifelse("expgroup1"=="expgroup2", TRUE, FALSE))
+
+blast <- subset(past, match == TRUE)
+
+ggplot(last, aes(trt_comp, value))+
+  facet_wrap(~expgroup1)+
+  geom_boxplot()+
+  theme_bw()
+
+
+
+
+
+
 
 
 
