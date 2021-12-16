e2088842c1dcd19d70d4d6d374d9eace3905eaaa
Tamara Munkemuller <muenkemueller@web.de>
2021-12-16 14:26:59 +0100

    update 1

--- community assembly wimmers:losers/mpd_adapted.R
+++ community assembly wimmers:losers/mpd_adapted.R
@@ -1,3 +1,31 @@
+
+mpd_focal <- function (samp, dis, abundance.weighted = FALSE) 
+{
+  N <- dim(samp)[1]
+  mpd <- numeric(N)
+  for (i in 1:N) {
+    sppInSample <- names(samp[i, samp[i, ] > 0])
+    if (length(sppInSample) > 1) {
+      sample.dis <- dis[sppInSample, sppInSample]
+      if (abundance.weighted) {
+        sample.weights <- t(as.matrix(samp[i, sppInSample, 
+                                           drop = FALSE])) %*% as.matrix(samp[i, sppInSample, 
+                                                                              drop = FALSE])
+        mpd[i] <- weighted.mean(sample.dis, sample.weights)
+      }
+      else {
+        mpd[i] <- mean(sample.dis[lower.tri(sample.dis)])
+      }
+    }
+    else {
+      mpd[i] <- NA
+    }
+  }
+  mpd
+}
+
+
+###### original function
 function (samp, dis, abundance.weighted = FALSE) 
 {
   N <- dim(samp)[1]
