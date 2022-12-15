1be3945b073cbc6c4d84586322436bd1dc28a68a
klapierre <kjkomatsu@uncg.edu>
2022-12-15 10:05:02 -0500

    Pretty the figures.

--- Phylogenetic Analyses/7_sCoRRE_phylogeneticDiversity_CWMtraits.R
+++ Phylogenetic Analyses/7_sCoRRE_phylogeneticDiversity_CWMtraits.R
@@ -16,74 +16,30 @@ setwd('C:\\Users\\kjkomatsu\\Dropbox (Smithsonian)\\working groups\\CoRRE\\sDiv\
 
 
 #functional diversity data
-fDiv <- read.csv('CoRRE_functionalDiversity_2022-12-13.csv')
-
-#treatment data
-trt <- read.csv('C:\\Users\\kjkomatsu\\Dropbox (Smithsonian)\\working groups\\CoRRE\\CoRRE_database\\Data\\CompiledData\\RawAbundance.csv') %>%
-  select(site_code, project_name, community_type, treatment_year, calendar_year, treatment, plot_id) %>%
-  unique() %>%
-  left_join(read.csv('C:\\Users\\kjkomatsu\\Dropbox (Smithsonian)\\working groups\\CoRRE\\CoRRE_database\\Data\\CompiledData\\ExperimentInfo.csv')) %>%
-  group_by(site_code, project_name, community_type) %>%
-  mutate(experiment_length=max(treatment_year)) %>%
-  ungroup() %>%
-  select(site_code, project_name, community_type, treatment_year, calendar_year, treatment, plot_id, trt_type, experiment_length, plot_mani, n, p, CO2, precip, temp)
-
-#merge CWM and trt data
-allDiv <- fDiv %>% #functional metrics
-  select(plot_id, treatment_year, site_code, project_name, community_type, site_proj_comm, treatment, CWM.growth_form, CWM.photosynthetic_pathway, CWM.lifespan, CWM.clonal, CWM.mycorrhizal_type, CWM.n_fixation, CWM.leaf_C.N, CWM.LDMC, CWM.SLA, CWM.plant_height_vegetative, CWM.rooting_depth, CWM.seed_dry_mass) %>% 
-  left_join(trt) %>% #treatments
-  full_join(read.csv('C:\\Users\\kjkomatsu\\Dropbox (Smithsonian)\\working groups\\CoRRE\\CoRRE_database\\Data\\CompiledData\\siteBiotic.csv')) %>% #site anpp and regional richness
-  full_join(read.csv('C:\\Users\\kjkomatsu\\Dropbox (Smithsonian)\\working groups\\CoRRE\\CoRRE_database\\Data\\CompiledData\\siteLocationClimate.csv')) %>% #site MAP and MAT
-  mutate(site_proj_comm_trt=paste(site_proj_comm, treatment, sep='::')) %>% 
-  select(-PubLat, -PubLong, -Offset, -Location) %>%
-  filter(richness>3) #remove wetlands, which have very few species and therefore don't nicely fit into these response types (about 2000 data points)
-
-#selecting relevant treatments for analysis (high resource, high stress)
-trt_analysis <- trt %>%
-  mutate(alltrts=ifelse(trt_type %in% c("control", "CO2","CO2*temp", "mow_clip","burn","burn*graze","disturbance","burn*mow_clip","drought","drought*CO2*temp","drought*mow_clip","drought*temp*mow_clip","herb_removal","herb_removal*mow_clip","irr*CO2","irr*CO2*temp","irr*mow_clip","irr*herb_removal","irr*temp*mow_clip","N*CO2*temp","N*irr*CO2","N*irr*mow_clip","N*P*burn*graze", "mult_nutrient*irr","N*irr*CO2*temp", "N","mult_nutrient","N*P","P","N*CO2","N*mow_clip","N*burn","N*burn*graze","N*disturbance","P*burn*graze","P*burn*mow_clip","N*drought","N*herb_removal","P*herb_removal","N*irr","N*irr*temp","N*temp","mult_nutrient*temp","N*P*temp","mult_nutrient*mow_clip","N*burn*mow_clip","N*P*burn","N*P*mow_clip","P*burn","P*mow_clip","mult_nutrient*herb_removal","mult_nutrient*herb_removal*mow_clip","temp","temp*mow_clip","drought*temp","irr*temp","irr"),1,0)) %>%
-  filter(alltrts==1) %>%
-  mutate(dist=ifelse(trt_type %in% c("mow_clip","burn","burn*graze","disturbance","burn*mow_clip"), 1, 0), #unify codes across datasets
-         # tCO2=ifelse(trt_type %in% c("CO2"), 1, 0),
-         drought=ifelse(trt_type %in% c("drought"), 1, 0),
-         # therb_removal=ifelse(trt_type %in% c("herb_removal"), 1, 0),
-         irg=ifelse(trt_type %in% c("irr"), 1, 0),
-         # ttemp=ifelse(trt_type %in% c("temp"), 1, 0),
-         # tn=ifelse(trt_type %in% c("N"), 1, 0),
-         # tp=ifelse(trt_type %in% c("P"), 1, 0),
-         multtrts=ifelse(trt_type %in% c("CO2*temp", "burn*graze","burn*mow_clip","drought*CO2*temp","drought*mow_clip","drought*temp*mow_clip","herb_removal*mow_clip","irr*CO2","irr*CO2*temp","irr*mow_clip","irr*herb_removal","irr*temp*mow_clip","N*CO2*temp","N*irr*CO2","N*irr*mow_clip","N*P*burn*graze", "mult_nutrient*irr","N*irr*CO2*temp", "N*CO2","N*mow_clip","N*burn","N*burn*graze","N*disturbance","P*burn*graze","P*burn*mow_clip","N*drought","N*herb_removal","P*herb_removal","N*irr","N*irr*temp","N*temp","mult_nutrient*temp","N*P*temp","mult_nutrient*mow_clip","N*burn*mow_clip","N*P*burn","N*P*mow_clip","P*burn","P*mow_clip","mult_nutrient*herb_removal","mult_nutrient*herb_removal*mow_clip","temp*mow_clip","drought*temp","irr*temp","mult_nutrient","N*P"),1,0)) %>%
-  mutate(trt_type2=ifelse(dist==1, 'disturbance', ifelse(multtrts==1, 'multiple trts', trt_type))) %>%
-  select(site_code, project_name, community_type, treatment, alltrts, dist, drought, irg, multtrts, trt_type2) %>%
-  unique()
-
-#pick treatments here
-allDivTrt <- allDiv %>%
-  right_join(trt_analysis) %>%
-  mutate(trt_binary=ifelse(plot_mani>0, 1, 0)) %>%
-  filter(!(is.na(site_proj_comm)))
-
-# write.csv(allDivTrt, 'CoRRE_CWMtraits_12142022.csv')
+fDiv <- read.csv('CoRRE_allDiversityMetrics_phyFunAnalysis.csv')
 
 
 ##### calculate continuous response ratios #####
 #filter control plots
-controlCont <- allDivTrt %>% 
-  filter(trt_type=='control') %>%
+controlCont <- fDiv %>% 
+  filter(trt_type2=='control') %>%
   group_by(site_code, project_name, community_type, treatment_year) %>%
   summarize_at(vars(CWM.leaf_C.N, CWM.LDMC, CWM.SLA, CWM.plant_height_vegetative, CWM.rooting_depth, CWM.seed_dry_mass), list(ctl=mean), na.rm=T) %>% #average across plots and years
   ungroup()
 
-allDivRRCont <- allDivTrt %>%
-  filter(trt_type!='control') %>%
+allDivRRCont <- fDiv %>%
+  filter(trt_type2!='control') %>%
   left_join(controlCont) %>%
   mutate(CWM.leaf_C.N_RR=(CWM.leaf_C.N-CWM.leaf_C.N_ctl)/CWM.leaf_C.N_ctl, CWM.LDMC_RR=(CWM.LDMC-CWM.LDMC_ctl)/CWM.LDMC_ctl, CWM.SLA_RR=(CWM.SLA-CWM.SLA_ctl)/CWM.SLA_ctl, CWM.plant_height_vegetative_RR=(CWM.plant_height_vegetative-CWM.plant_height_vegetative_ctl)/CWM.plant_height_vegetative_ctl, CWM.rooting_depth_RR=(CWM.rooting_depth-CWM.rooting_depth_ctl)/CWM.rooting_depth_ctl, CWM.seed_dry_mass_RR=(CWM.seed_dry_mass-CWM.seed_dry_mass_ctl)/CWM.seed_dry_mass_ctl) %>% 
   mutate(site_proj_comm=paste(site_code, project_name, community_type, sep='::')) %>% 
   group_by(site_proj_comm, site_code, project_name, community_type, treatment, trt_type2, plot_id) %>%
   summarise_at(vars(CWM.leaf_C.N_RR, CWM.LDMC_RR, CWM.SLA_RR, CWM.plant_height_vegetative_RR, CWM.rooting_depth_RR, CWM.seed_dry_mass_RR), list(mean=mean), na.rm=T) %>%
-  ungroup()
+  ungroup() %>% 
+  filter(!is.nan(CWM.leaf_C.N_RR_mean))
 
 ##### checking continuous trait data #####
-hist(allDivRRCont$CWM.leaf_C.N_RR)
-qqPlot(allDivRRCont$CWM.leaf_C.N_RR)
+hist(allDivRRCont$CWM.leaf_C.N_RR_mean)
+qqPlot(allDivRRCont$CWM.leaf_C.N_RR_mean)
 
 hist(allDivRRCont$CWM.LDMC_RR_mean)
 qqPlot(allDivRRCont$CWM.LDMC_RR_mean)
@@ -111,7 +67,7 @@ library(grid)
 options(contrasts=c('contr.sum','contr.poly')) 
 
 #leaf C:N model
-summary(cnModel <- lme(CWM.leaf_C.N_RR ~ as.factor(trt_type2),
+summary(cnModel <- lme(CWM.leaf_C.N_RR_mean ~ as.factor(trt_type2),
                         data=na.omit(subset(allDivRRCont, trt_type2!='herb_removal')),
                         random=~1|site_proj_comm))
 anova.lme(cnModel, type='sequential')
@@ -231,19 +187,292 @@ print(rootFig, vp=viewport(layout.pos.row=3, layout.pos.col=2))
 
 ##### calculate categorical trait responses #####
 #filter control plots
-controlCat <- allDivTrt %>% 
-  filter(trt_type=='control') %>%
+controlCat <- fDiv %>% 
+  filter(trt_type2=='control') %>%
   group_by(site_code, project_name, community_type, treatment_year) %>%
   summarize_at(vars(CWM.growth_form, CWM.photosynthetic_pathway, CWM.lifespan, CWM.clonal, CWM.mycorrhizal_type, CWM.n_fixation), list(ctl=max), na.rm=T) %>% #average aross plots and years
   ungroup()
 
-allDivRRCat <- allDivTrt %>% 
-  left_join(controlCat) 
+allDivRRCat <- fDiv %>% 
+  filter(!(trt_type2=='control')) %>% 
+  left_join(controlCat) %>% 
+  select(site_code, project_name, community_type, site_proj_comm, treatment_year, trt_type2, treatment, plot_id, CWM.growth_form:CWM.n_fixation, CWM.growth_form_ctl:CWM.n_fixation_ctl) %>% 
+  rename(CWM.growthform_trt=CWM.growth_form, 
+         CWM.growthform_ctl=CWM.growth_form_ctl,
+         CWM.photosyntheticpathway_trt=CWM.photosynthetic_pathway, 
+         CWM.photosyntheticpathway_ctl=CWM.photosynthetic_pathway_ctl,
+         CWM.lifespan_trt=CWM.lifespan,
+         CWM.clonal_trt=CWM.clonal,
+         CWM.mycorrhizaltype_trt=CWM.mycorrhizal_type, 
+         CWM.mycorrhizaltype_ctl=CWM.mycorrhizal_type_ctl,
+         CWM.nfixation_trt=CWM.n_fixation,
+         CWM.nfixation_ctl=CWM.n_fixation_ctl) %>% 
+  group_by(site_code, project_name, community_type, site_proj_comm, trt_type2, treatment, plot_id) %>% 
+  summarize_at(vars(CWM.growthform_trt, CWM.photosyntheticpathway_trt, CWM.lifespan_trt, CWM.clonal_trt, CWM.mycorrhizaltype_trt, CWM.nfixation_trt, CWM.growthform_ctl, CWM.photosyntheticpathway_ctl, CWM.lifespan_ctl, CWM.clonal_ctl, CWM.mycorrhizaltype_ctl, CWM.nfixation_ctl), list(max), na.rm=T) %>% #average aross plots and years
+  ungroup()
+
+##### individual categorical trait figures #####
+### growth form ###
+growthform <- allDivRRCat %>% 
+  select(site_code, project_name, community_type, site_proj_comm, trt_type2, treatment, plot_id, CWM.growthform_trt, CWM.growthform_ctl)
+
+#N
+growthformN <- growthform %>% 
+  filter(trt_type2=='N') %>% 
+  rename(trt=CWM.growthform_trt, ctl=CWM.growthform_ctl) %>% 
+  pivot_longer(cols=trt:ctl, names_to='trtctl') %>% 
+  mutate(value2=ifelse(value=='graminoid', 1, 0))
+
+#P
+growthformP <- growthform %>% 
+  filter(trt_type2=='P') %>% 
+  rename(trt=CWM.growthform_trt, ctl=CWM.growthform_ctl) %>% 
+  pivot_longer(cols=trt:ctl, names_to='trtctl') %>% 
+  mutate(value2=ifelse(value=='graminoid', 1, 0))
+
+#irr
+growthformIrr <- growthform %>% 
+  filter(trt_type2=='irr') %>% 
+  rename(trt=CWM.growthform_trt, ctl=CWM.growthform_ctl) %>% 
+  pivot_longer(cols=trt:ctl, names_to='trtctl') %>% 
+  mutate(value2=ifelse(value=='graminoid', 1, 0))
+
+#CO2
+growthformCO2 <- growthform %>% 
+  filter(trt_type2=='CO2') %>% 
+  rename(trt=CWM.growthform_trt, ctl=CWM.growthform_ctl) %>% 
+  pivot_longer(cols=trt:ctl, names_to='trtctl') %>% 
+  mutate(value2=ifelse(value=='graminoid', 1, 0))
+
+#drought
+growthformDrought <- growthform %>% 
+  filter(trt_type2=='drought') %>% 
+  rename(trt=CWM.growthform_trt, ctl=CWM.growthform_ctl) %>% 
+  pivot_longer(cols=trt:ctl, names_to='trtctl') %>% 
+  mutate(value2=ifelse(value=='graminoid', 1, 0))
+
+#temp
+growthformTemp <- growthform %>% 
+  filter(trt_type2=='temp') %>% 
+  rename(trt=CWM.growthform_trt, ctl=CWM.growthform_ctl) %>% 
+  pivot_longer(cols=trt:ctl, names_to='trtctl') %>% 
+  mutate(value2=ifelse(value=='graminoid', 1, 0))
+
+#disturbance
+growthformDisturbance <- growthform %>% 
+  filter(trt_type2=='disturbance') %>% 
+  rename(trt=CWM.growthform_trt, ctl=CWM.growthform_ctl) %>% 
+  pivot_longer(cols=trt:ctl, names_to='trtctl') %>% 
+  mutate(value2=ifelse(value=='graminoid', 1, 0))
+
+#multiple trts
+growthformMultiple <- growthform %>% 
+  filter(trt_type2=='multiple trts') %>% 
+  rename(trt=CWM.growthform_trt, ctl=CWM.growthform_ctl) %>% 
+  pivot_longer(cols=trt:ctl, names_to='trtctl') %>% 
+  mutate(value2=ifelse(value=='graminoid', 1, 0))
+
+
+#rbind for all growth form
+growthform2 <- rbind(growthformN, growthformP, growthformIrr, growthformCO2, growthformDrought, growthformTemp, growthformDisturbance, growthformMultiple)
+
+#figure
+growthformFig <- ggplot(data=growthform2, aes(x=trt_type2, y=value2, color=trtctl)) +
+  geom_jitter(height=0.1) +
+  stat_summary(
+    geom = "linerange",
+    fun.data = mean_sdl, 
+    fun.args = list(mult = 1),
+    aes(colour = trtctl),
+    position=position_dodge(0.4)
+  ) +
+  stat_summary(
+    geom = "point",
+    fun = mean,
+    aes(colour = trtctl), size = 3,
+    position=position_dodge(0.4)
+  ) +
+  ylab('Growth Form') +
+  theme(axis.title.x=element_blank(),
+        axis.text.x=element_text(angle=90, vjust=0.5, hjust=1)) +
+  scale_color_manual(values=c('grey', 'dark orange'))
+
   
+### lifespan ###
+lifespan <- allDivRRCat %>% 
+  select(site_code, project_name, community_type, site_proj_comm, trt_type2, treatment, plot_id, CWM.lifespan_trt, CWM.lifespan_ctl)
+
+#N
+lifespanN <- lifespan %>% 
+  filter(trt_type2=='N') %>% 
+  rename(trt=CWM.lifespan_trt, ctl=CWM.lifespan_ctl) %>% 
+  pivot_longer(cols=trt:ctl, names_to='trtctl') %>% 
+  mutate(value2=ifelse(value=='perennial', 1, 0))
+
+#P
+lifespanP <- lifespan %>% 
+  filter(trt_type2=='P') %>% 
+  rename(trt=CWM.lifespan_trt, ctl=CWM.lifespan_ctl) %>% 
+  pivot_longer(cols=trt:ctl, names_to='trtctl') %>% 
+  mutate(value2=ifelse(value=='perennial', 1, 0))
+
+#irr
+lifespanIrr <- lifespan %>% 
+  filter(trt_type2=='irr') %>% 
+  rename(trt=CWM.lifespan_trt, ctl=CWM.lifespan_ctl) %>% 
+  pivot_longer(cols=trt:ctl, names_to='trtctl') %>% 
+  mutate(value2=ifelse(value=='perennial', 1, 0))
+
+#CO2
+lifespanCO2 <- lifespan %>% 
+  filter(trt_type2=='CO2') %>% 
+  rename(trt=CWM.lifespan_trt, ctl=CWM.lifespan_ctl) %>% 
+  pivot_longer(cols=trt:ctl, names_to='trtctl') %>% 
+  mutate(value2=ifelse(value=='perennial', 1, 0))
+
+#drought
+lifespanDrought <- lifespan %>% 
+  filter(trt_type2=='drought') %>% 
+  rename(trt=CWM.lifespan_trt, ctl=CWM.lifespan_ctl) %>% 
+  pivot_longer(cols=trt:ctl, names_to='trtctl') %>% 
+  mutate(value2=ifelse(value=='perennial', 1, 0))
+
+#temp
+lifespanTemp <- lifespan %>% 
+  filter(trt_type2=='temp') %>% 
+  rename(trt=CWM.lifespan_trt, ctl=CWM.lifespan_ctl) %>% 
+  pivot_longer(cols=trt:ctl, names_to='trtctl') %>% 
+  mutate(value2=ifelse(value=='perennial', 1, 0))
+
+#disturbance
+lifespanDisturbance <- lifespan %>% 
+  filter(trt_type2=='disturbance') %>% 
+  rename(trt=CWM.lifespan_trt, ctl=CWM.lifespan_ctl) %>% 
+  pivot_longer(cols=trt:ctl, names_to='trtctl') %>% 
+  mutate(value2=ifelse(value=='perennial', 1, 0))
+
+#multiple trts
+lifespanMultiple <- lifespan %>% 
+  filter(trt_type2=='multiple trts') %>% 
+  rename(trt=CWM.lifespan_trt, ctl=CWM.lifespan_ctl) %>% 
+  pivot_longer(cols=trt:ctl, names_to='trtctl') %>% 
+  mutate(value2=ifelse(value=='perennial', 1, 0))
+
+
+#rbind for all lifespan
+lifespan2 <- rbind(lifespanN, lifespanP, lifespanIrr, lifespanCO2, lifespanDrought, lifespanTemp, lifespanDisturbance, lifespanMultiple)
+
+#figure
+lifespanFig <- ggplot(data=lifespan2, aes(x=trt_type2, y=value2, color=trtctl)) +
+  geom_jitter(height=0.1) +
+  stat_summary(
+    geom = "linerange",
+    fun.data = mean_sdl, 
+    fun.args = list(mult = 1),
+    aes(colour = trtctl),
+    position=position_dodge(0.4)
+  ) +
+  stat_summary(
+    geom = "point",
+    fun = mean,
+    aes(colour = trtctl), size = 3,
+    position=position_dodge(0.4)
+  ) +
+  ylab('Lifespan') +
+  theme(axis.title.x=element_blank(),
+        axis.text.x=element_text(angle=90, vjust=0.5, hjust=1)) +
+  scale_color_manual(values=c('grey', 'dark orange'))
+
   
+### mycorrhizaltype ###
+mycorrhizaltype <- allDivRRCat %>% 
+  select(site_code, project_name, community_type, site_proj_comm, trt_type2, treatment, plot_id, CWM.mycorrhizaltype_trt, CWM.mycorrhizaltype_ctl)
+
+#N
+mycorrhizaltypeN <- mycorrhizaltype %>% 
+  filter(trt_type2=='N') %>% 
+  rename(trt=CWM.mycorrhizaltype_trt, ctl=CWM.mycorrhizaltype_ctl) %>% 
+  pivot_longer(cols=trt:ctl, names_to='trtctl') %>% 
+  mutate(value2=ifelse(value=='yes', 1, 0))
+
+#P
+mycorrhizaltypeP <- mycorrhizaltype %>% 
+  filter(trt_type2=='P') %>% 
+  rename(trt=CWM.mycorrhizaltype_trt, ctl=CWM.mycorrhizaltype_ctl) %>% 
+  pivot_longer(cols=trt:ctl, names_to='trtctl') %>% 
+  mutate(value2=ifelse(value=='yes', 1, 0))
+
+#irr
+mycorrhizaltypeIrr <- mycorrhizaltype %>% 
+  filter(trt_type2=='irr') %>% 
+  rename(trt=CWM.mycorrhizaltype_trt, ctl=CWM.mycorrhizaltype_ctl) %>% 
+  pivot_longer(cols=trt:ctl, names_to='trtctl') %>% 
+  mutate(value2=ifelse(value=='yes', 1, 0))
+
+#CO2
+mycorrhizaltypeCO2 <- mycorrhizaltype %>% 
+  filter(trt_type2=='CO2') %>% 
+  rename(trt=CWM.mycorrhizaltype_trt, ctl=CWM.mycorrhizaltype_ctl) %>% 
+  pivot_longer(cols=trt:ctl, names_to='trtctl') %>% 
+  mutate(value2=ifelse(value=='yes', 1, 0))
+
+#drought
+mycorrhizaltypeDrought <- mycorrhizaltype %>% 
+  filter(trt_type2=='drought') %>% 
+  rename(trt=CWM.mycorrhizaltype_trt, ctl=CWM.mycorrhizaltype_ctl) %>% 
+  pivot_longer(cols=trt:ctl, names_to='trtctl') %>% 
+  mutate(value2=ifelse(value=='yes', 1, 0))
+
+#temp
+mycorrhizaltypeTemp <- mycorrhizaltype %>% 
+  filter(trt_type2=='temp') %>% 
+  rename(trt=CWM.mycorrhizaltype_trt, ctl=CWM.mycorrhizaltype_ctl) %>% 
+  pivot_longer(cols=trt:ctl, names_to='trtctl') %>% 
+  mutate(value2=ifelse(value=='yes', 1, 0))
+
+#disturbance
+mycorrhizaltypeDisturbance <- mycorrhizaltype %>% 
+  filter(trt_type2=='disturbance') %>% 
+  rename(trt=CWM.mycorrhizaltype_trt, ctl=CWM.mycorrhizaltype_ctl) %>% 
+  pivot_longer(cols=trt:ctl, names_to='trtctl') %>% 
+  mutate(value2=ifelse(value=='yes', 1, 0))
+
+#multiple trts
+mycorrhizaltypeMultiple <- mycorrhizaltype %>% 
+  filter(trt_type2=='multiple trts') %>% 
+  rename(trt=CWM.mycorrhizaltype_trt, ctl=CWM.mycorrhizaltype_ctl) %>% 
+  pivot_longer(cols=trt:ctl, names_to='trtctl') %>% 
+  mutate(value2=ifelse(value=='yes', 1, 0))
+
+
+#rbind for all mycorrhizaltype
+mycorrhizaltype2 <- rbind(mycorrhizaltypeN, mycorrhizaltypeP, mycorrhizaltypeIrr, mycorrhizaltypeCO2, mycorrhizaltypeDrought, mycorrhizaltypeTemp, mycorrhizaltypeDisturbance, mycorrhizaltypeMultiple)
+
+#figure
+mycorrhizaltypeFig <- ggplot(data=mycorrhizaltype2, aes(x=trt_type2, y=value2, color=trtctl)) +
+  geom_jitter(height=0.1) +
+  stat_summary(
+    geom = "linerange",
+    fun.data = mean_sdl, 
+    fun.args = list(mult = 1),
+    aes(colour = trtctl),
+    position=position_dodge(0.4)
+  ) +
+  stat_summary(
+    geom = "point",
+    fun = mean,
+    aes(colour = trtctl), size = 3,
+    position=position_dodge(0.4)
+  ) +
+  ylab('mycorrhizaltype') +
+  theme(axis.title.x=element_blank(),
+        axis.text.x=element_text(angle=90, vjust=0.5, hjust=1)) +
+  scale_color_manual(values=c('grey', 'dark orange'))
+
   
-  
-  
-  
-  
-  
+#didn't finish making these figs, there's three more traits to go
+pushViewport(viewport(layout=grid.layout(1,3)))
+print(growthformFig, vp=viewport(layout.pos.row=1, layout.pos.col=1))
+print(lifespanFig, vp=viewport(layout.pos.row=1, layout.pos.col=2))
+print(mycorrhizaltypeFig, vp=viewport(layout.pos.row=1, layout.pos.col=3))
+#export 1800x800
