c605b04a445d190817b9bfa487897a50ee85c689
Meghan Avolio <meghan.avolio@gmail.com>
2022-12-15 09:29:50 -0500

    making pie chart of responses for each family we want to highlight

--- winners losers/PieCharts.R
+++ winners losers/PieCharts.R
@@ -1,25 +1,86 @@
 library(tidyverse)
 
+#run pie2_function first
+
 trt<-c("CO2", "Drt.","Irg.", "Temp.", "N", "P", "Mult. Nuts.","Inter.")
 value<-c(1,1,1,1,1,1,1,1)
-fill<-c("Black", "Purple", "Darkgreen", "Green", "Blue", "Goldenro3", "Darkgray", "Orange")
-pie<-data.frame(trt, value, fill)
-#using ggplot - not sure i want to do this
-ggplot(data=pie, aes(x=="",y=value, fill=trt))+
-  geom_bar(stat = "identity")+
-  coord_polar("x",start=0)+
-  scale_fill_manual(values=fill)
+pie<-data.frame(trt, value)
 
 ##doing this in baseR
 collst = RColorBrewer::brewer.pal(n = 8, name = "Dark2")
 
-#make base pie chart 
+#make Legend
 pie(pie$value, labels=pie$trt, col=collst, border="white")
 
 #for poaceae
-labellistpoa<-c("+", "+","+","", "","-","","")
+labellistpoa<-c("", "-","","", "+","","+","+")
 colactive<-collst
-colactive[labellist==""]="darkgray"
+colactive[labellistpoa==""]="darkgray"
 pie2(pie$value, labels=labellistpoa, col=colactive, border="white",line_length = 1, text_center = 0.6, textcol = "white")
 
+#for cyperaceae
+labellistcyp<-c("", "","","", "","","+","+")
+colactive<-collst
+colactive[labellistcyp==""]="darkgray"
+pie2(pie$value, labels=labellistcyp, col=colactive, border="white",line_length = 1, text_center = 0.6, textcol = "white")
 
+#for orchidaceae
+labellistorc<-c("", "","","", "","","-","")
+colactive<-collst
+colactive[labellistorc==""]="darkgray"
+pie2(pie$value, labels=labellistorc, col=colactive, border="white",line_length = 1, text_center = 0.6, textcol = "white")
+
+#for asteraceae
+labellistast<-c("", "","+","", "-","","","")
+colactive<-collst
+colactive[labellistast==""]="darkgray"
+pie2(pie$value, labels=labellistast, col=colactive, border="white",line_length = 1, text_center = 0.6, textcol = "white")
+
+#for Lamiacea and Orobanaceae and Polomonaceae
+labellistLO<-c("", "","","", "-","","","")
+colactive<-collst
+colactive[labellistLO==""]="darkgray"
+pie2(pie$value, labels=labellistLO, col=colactive, border="white",line_length = 1, text_center = 0.6, textcol = "white")
+
+#for Plantaginaceae
+labellistpla<-c("", "","","", "","","","-")
+colactive<-collst
+colactive[labellistpla==""]="darkgray"
+pie2(pie$value, labels=labellistpla, col=colactive, border="white",line_length = 1, text_center = 0.6, textcol = "white")
+
+#for Gentinaceae
+labellistgen<-c("", "","","", "-","","-","-")
+colactive<-collst
+colactive[labellistgen==""]="darkgray"
+pie2(pie$value, labels=labellistgen, col=colactive, border="white",line_length = 1, text_center = 0.6, textcol = "white")
+
+#for Solaneaceae
+labellistsol<-c("", "","","", "+","","+","")
+colactive<-collst
+colactive[labellistsol==""]="darkgray"
+pie2(pie$value, labels=labellistsol, col=colactive, border="white",line_length = 1, text_center = 0.6, textcol = "white")
+
+#for Amaranth
+labellistama<-c("", "+","-","", "+","-","","")
+colactive<-collst
+colactive[labellistama==""]="darkgray"
+pie2(pie$value, labels=labellistama, col=colactive, border="white",line_length = 1, text_center = 0.6, textcol = "white")
+
+#for Fabaceae
+labellistfab<-c("", "-","+","", "-","","-","")
+colactive<-collst
+colactive[labellistfab==""]="darkgray"
+pie2(pie$value, labels=labellistfab, col=colactive, border="white",line_length = 1, text_center = 0.6, textcol = "white")
+
+#for Euphorb
+labellisteup<-c("", "","","+", "","","","")
+colactive<-collst
+colactive[labellisteup==""]="darkgray"
+pie2(pie$value, labels=labellisteup, col=colactive, border="white",line_length = 1, text_center = 0.6, textcol = "white")
+
+
+#for Brassicaceae
+labellistbra<-c("", "","","", "","","","+")
+colactive<-collst
+colactive[labellistbra==""]="darkgray"
+pie2(pie$value, labels=labellistbra, col=colactive, border="white",line_length = 1, text_center = 0.6, textcol = "white")
