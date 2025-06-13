19a73593de6b141a8efa83c0248f8c1328b2bc57
klapierre <kjkomatsu@uncg.edu>
2025-06-13 09:17:20 -0400

    Calculating functional diversity with subset data.

--- PhyDiv_FuncDiv/3_sCoRRE_weighted_functionalDiversityMetrics.R
+++ PhyDiv_FuncDiv/3_sCoRRE_weighted_functionalDiversityMetrics.R
@@ -21,6 +21,15 @@ se <- function(x, na.rm=na.rm){
 
 ##### data import and cleaning #####
 
+#spp names
+names <- readRDS('PhyDiv_FuncDiv/sppList.rds')
+
+#community data
+comm <- readRDS('PhyDiv_FuncDiv/PD_FD_commData.rds')
+
+#treatment data
+trt <- readRDS('PhyDiv_FuncDiv/trt_info.rds')
+
 # trait data
 correGExTraitsContinuous <- read.csv('https://pasta.lternet.edu/package/data/eml/edi/1533/3/169fc12d10ac20b0e504f8d5ca0b8ee8') %>% 
   select(-family, -source, -imputation_error, -error_risk_overall, -error_risk_family, -error_risk_genus)
@@ -36,7 +45,6 @@ traits <- rbind(correGExTraitsCategorical, correGExTraitsContinuous) %>%
                 as.factor),
          across(c(LDMC, SLA, SRL, leaf_N, plant_height_vegetative, seed_dry_mass), 
                 as.numeric)) %>% 
-  rename(species_matched=species) %>% 
   na.omit() #only keep trait data that is complete for all traits (drops 1157 species with only categorical trait data, 28.4% of species)
 
 
@@ -105,108 +113,69 @@ shapiro.test(traitsScaled$seed_dry_mass)
 #log W = 0.99368, p-value = 5.985e-10
 
 
-##### relative cover datasets #####
-
-# species relative cover data
-relCoverRaw <- read.csv("C:\\Users\\kjkomatsu\\Smithsonian Dropbox\\Kimberly Komatsu\\working groups\\CoRRE\\CoRRE_database\\Data\\CompiledData\\RelativeCoverMarch2024.csv") %>%
-  mutate(site_proj_comm = paste(site_code, project_name, community_type, sep="_")) %>%
-  mutate(plot_id=ifelse(project_name=='NSFC', paste(plot_id, treatment, sep='__'), plot_id)) %>% 
-  mutate(plot_id=ifelse(project_name=='IRG', paste(block, plot_id, sep='__'), plot_id)) %>% 
-  select(site_code:community_type, site_proj_comm, calendar_year:relcov) %>% 
-  mutate(drop=ifelse(site_code=="CDR"&treatment %in% c(2, 3, 4, 5, 7), 1, 0)) %>% #drop some of the CDR e001 and e002 treatments to prevent over-representation
-  filter(drop==0) %>% 
-  select(-drop)
-  
-
-# corre to try species names key
-corre_to_try <- read.csv("C:\\Users\\kjkomatsu\\Smithsonian Dropbox\\Kimberly Komatsu\\working groups\\CoRRE\\CoRRE_database\\Data\\CompiledData\\corre2trykey_2021.csv") %>%
-  select(genus_species, species_matched) %>%
-  unique()
-
-relCovClean <- relCoverRaw %>%
-  left_join(corre_to_try, by="genus_species") %>%
-  mutate(species_matched=ifelse(is.na(species_matched), genus_species, species_matched)) %>% 
-  select(-genus_species)
-
-
-##### treatment data #####
-trt <- read.csv('C:\\Users\\kjkomatsu\\Smithsonian Dropbox\\Kimberly Komatsu\\working groups\\CoRRE\\CoRRE_database\\Data\\CompiledData\\RelativeCoverMarch2024.csv') %>%
-  mutate(plot_id=ifelse(project_name=='NSFC', paste(plot_id, treatment, sep='__'), plot_id)) %>% 
-  mutate(plot_id=ifelse(project_name=='IRG', paste(block, plot_id, sep='__'), plot_id)) %>% 
-  select(site_code, project_name, community_type, treatment_year, calendar_year, treatment, plot_id) %>%
-  unique() %>%
-  left_join(read.csv('C:\\Users\\kjkomatsu\\Smithsonian Dropbox\\Kimberly Komatsu\\working groups\\CoRRE\\CoRRE_database\\Data\\CompiledData\\ExperimentInfo_March2024.csv'),
-            relationship = "many-to-many") %>%
-  group_by(site_code, project_name, community_type) %>%
-  mutate(experiment_length=max(treatment_year)) %>%
-  ungroup() %>%
-  select(site_code, project_name, community_type, treatment_year, calendar_year, treatment, plot_id, trt_type, experiment_length, plot_mani, n, p, CO2, precip, temp) %>% 
-  mutate(site_proj_comm=paste(site_code, project_name, community_type, sep='_'))
-
-
 ##### calculate functional dispersion - loop through sites #####
 functionalDiversityMetrics <- {}
-site_vector <- unique(relCovClean$site_code) # do this for site_code only to reshuffle accurately
+site_vector <- unique(comm$site_code) # do this for site_code only to reshuffle accurately
 
 for(s in 1:length(site_vector)){
   
   print(s*100/length(site_vector))
   
   #relative cover data from each site
-  relCoverSubset <- relCovClean %>%
+  relCoverSubset <- comm %>%
     filter(site_code==site_vector[s])
   
   #species vector for pulling traits from relative cover
-  sppSubset <- data.frame(species_matched = unique(relCoverSubset$species_matched), dummy=1) 
+  sppSubset <- data.frame(species = unique(relCoverSubset$species), dummy=1) 
   
   sppSubsetVector <- sppSubset %>%
     na.omit() %>% 
-    pull(species_matched) %>% 
+    pull(species) %>% 
     unique()
   
   #subset trait data to just include species in the relative cover data
   traitsSubset <- traitsScaled %>%
-    filter(species_matched %in% sppSubsetVector)
+    filter(species %in% sppSubsetVector)
   
   #dataframe with species present in both the trait database and the relative cover data base
-  speciesSubsetKeep <- data.frame(species_matched = unique(traitsSubset$species_matched),
+  speciesSubsetKeep <- data.frame(species = unique(traitsSubset$species),
                                   dummy_traits=2) %>%
-    arrange(species_matched)
+    arrange(species)
   
   #vector of species not in trait database (but in relative abundance data) to remove from species abundance data
   speciesSubsetRemove <- sppSubset %>%
-    full_join(speciesSubsetKeep, by="species_matched") %>%
+    full_join(speciesSubsetKeep, by="species") %>%
     filter(is.na(dummy_traits)) %>%
-    pull(species_matched)
+    pull(species)
   
   #abundance dataset with species removed that do not have trait information
   relCoverSubsetKeep <- relCoverSubset %>%
-    filter(!species_matched %in% speciesSubsetRemove) #removing species without trait information
+    filter(!species %in% speciesSubsetRemove) #removing species without trait information
   
   #abundance data into wide format
   relCoverWideSubset <- relCoverSubsetKeep %>%
-    group_by(site_code, project_name, community_type, site_proj_comm, calendar_year, treatment_year, treatment,
-             block, plot_id, data_type, version, species_matched) %>%
+    group_by(site_code, project_name, community_type, 
+             calendar_year, treatment_year, treatment,
+             block, plot_id, plot_id2, species) %>%
     summarize(relcov=sum(relcov, na.rm=T)) %>%
     ungroup() %>%
-    spread(key=species_matched, value=relcov) %>%
+    spread(key=species, value=relcov) %>%
     replace(is.na(.), 0)
   
   #plot information
   plotInfoSubset <- relCoverWideSubset %>%
-    select(site_code:version)
+    select(site_code:plot_id2)
   
   #cover data
   relCoverWideSubset2 <- relCoverWideSubset %>%
-    mutate(identifier=paste(plotInfoSubset$site_proj_comm, plotInfoSubset$calendar_year,
-                            plotInfoSubset$plot_id, sep="::")) %>% 
-    select(-site_code:-version) %>% 
+    mutate(identifier=paste(plotInfoSubset$site_code, plotInfoSubset$project_name, plotInfoSubset$community_type, plotInfoSubset$calendar_year,                            plotInfoSubset$plot_id, sep="::")) %>% 
+    select(-site_code:-plot_id2) %>% 
     column_to_rownames("identifier") 
   
   #dbFD function requires species names in trait data frame be arranged A-Z and identical order to the abundance data 
   traitsSubsetArranged <- traitsSubset %>%
-    arrange(species_matched) %>%
-    column_to_rownames("species_matched")
+    arrange(species) %>%
+    column_to_rownames("species")
   
   ### Calculate functional diversity metrics ###
   relCoverMatrixSubset <- as.matrix(relCoverWideSubset2)
@@ -215,7 +184,7 @@ for(s in 1:length(site_vector)){
   # RaoQ
   rao_q <- fd_raoq(sp_com=relCoverMatrixSubset, dist_matrix=traitMatrixSubset) %>%
     rename(identifier=site) %>% 
-    separate(identifier, into=c("site_proj_comm", "calendar_year","plot_id"), sep="::") %>%
+    separate(identifier, into=c("site_code", "project_name", "community_type", "calendar_year","plot_id"), sep="::") %>%
     mutate(calendar_year = as.numeric(calendar_year),
            permutation=0)
   
@@ -248,16 +217,16 @@ for(s in 1:length(site_vector)){
       column_to_rownames(var="spp_shuffling")
     
     traitsSubsetArrangedSES <- traitsSubsetSES %>%
-      rownames_to_column("species_matched") %>% 
-      arrange(species_matched) %>%
-      column_to_rownames("species_matched")
+      rownames_to_column("species") %>% 
+      arrange(species) %>%
+      column_to_rownames("species")
     
     traitMatrixSubsetSES <- as.matrix(daisy(traitsSubsetArrangedSES, metric = "gower"))
     
     ses2 <- fd_raoq(sp_com=relCoverMatrixSubset, 
                          dist_matrix=traitMatrixSubsetSES) %>%
       rename(identifier=site) %>% 
-      separate(identifier, into=c("site_proj_comm", "calendar_year","plot_id"), sep="::") %>%
+      separate(identifier, into=c("site_code", "project_name", "community_type", "calendar_year","plot_id"), sep="::") %>%
       mutate(calendar_year = as.numeric(calendar_year),
              permutation=sesVector[n])
     
@@ -282,7 +251,7 @@ for(s in 1:length(site_vector)){
   
   #mean RaoQ for null distribution to create SES RaoQ
   sesSubsetMean <- ses %>% 
-    group_by(site_proj_comm, calendar_year, plot_id) %>% 
+    group_by(site_code, project_name, community_type, calendar_year, plot_id) %>% 
     summarise(across(c('Q'), list(mean=mean, sd=sd))) %>% 
     ungroup()
   
@@ -290,9 +259,10 @@ for(s in 1:length(site_vector)){
   raoqSubsetSES <- rao_q %>% 
     full_join(sesSubsetMean) %>% 
     mutate(RaoQ_ses=(Q-Q_mean)/Q_sd) %>% 
-    select(site_proj_comm, calendar_year, plot_id, RaoQ_ses)
+    select(site_code, project_name, community_type, calendar_year, plot_id, RaoQ_ses)
   
   allSubset <- rao_q %>% 
+    select(-permutation) %>% 
     full_join(raoqSubsetSES) %>% 
     left_join(trt)
   
